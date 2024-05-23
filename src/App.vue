bf4b9d85f2b5c5dce9b275505e7d869106b3eadb
Bob Fornal <bob.fornal@leadingedje.com>
2024-05-23 10:58:45 -0400

    Base Application

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { computed, ref } from 'vue';
+
+  const active = ref(false);
+  const activeString = computed(() => `${active.value}`);
+
+  function toggleActive() {
+    active.value = active.value === 'true' ? 'false' : 'true';
+  }
+</script>
+
+<template>
+  <main>
+    <edje-spinner :active="activeString"></edje-spinner>
+    <div style="position: fixed; top: 1em; left: 1em; z-index: 200000;">
+      <button @click="toggleActive">Toggle</button>
+    </div>
+  </main>
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css';
+import './assets/edje-spinner/edje-spinner.css';
+import './assets/edje-spinner/edje-spinner.min.js';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
